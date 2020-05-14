3404ea8cae793d4823ff29359cbdd37dbbf94c97
devleandrodias <leandrodbdias@gmail.com>
2020-05-14 17:16:51 -0300

    modificadores de acesso, public, protected, private

---
+++ modificadores_acesso.php
@@ -0,0 +1,13 @@
+<?php
+
+class Carro
+{
+  public $nome;
+  protected $modelo;
+  private $numeroFabrica;
+
+  public function __construct()
+  {
+    $this->numeroFabrica = 'fsfj-2342s-sdfdsf-r24-fs';
+  }
+}
