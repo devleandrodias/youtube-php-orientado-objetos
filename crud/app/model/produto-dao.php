3c8191ec0e0f657b168625b4687361fe01f26c77
devleandrodias <leandrodbdias@gmail.com>
2020-05-15 14:41:39 -0300

    finalizacao do curso de orinetacao a objetos em php

--- crud/app/model/produto-dao.php
+++ crud/app/model/produto-dao.php
@@ -5,6 +5,7 @@ namespace App\Model;
 require 'conexao.php';
 
 use App\Model\Produto;
+use PDO;
 
 class ProdutoDAO
 {
@@ -14,24 +15,42 @@ class ProdutoDAO
 
     $stmt = Conexao::obterConexao()->prepare($sql);
 
-    $stmt->bindValue(1, $produto->getNome);
-    $stmt->bindValue(2, $produto->getDescricao);
+    $stmt->bindValue(1, $produto->getNome());
+    $stmt->bindValue(2, $produto->getDescricao());
 
     $stmt->execute();
   }
 
   public function read()
   {
-    # code...
+    $sql = "SELECT * FROM produtos";
+    $stmt = Conexao::obterConexao()->prepare($sql);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) :
+      $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $resultado;
+    else :
+      return [];
+    endif;
   }
 
   public function update(Produto $produto)
   {
-    # code...
+    $sql = 'UPDATE produtos SET nome = ?, descricao = ? WHERE id = ?';
+    $stmt = Conexao::obterConexao()->prepare($sql);
+
+    $stmt->bindValue(1, $produto->getNome());
+    $stmt->bindValue(2, $produto->getDescricao());
+    $stmt->bindValue(3, $produto->getId());
+
+    $stmt->execute();
   }
 
   public function delete($id)
   {
-    # code...
+    $sql = 'DELETE FROM produtos WHERE id = ?';
+    $stmt = Conexao::obterConexao()->prepare($sql);
+    $stmt->bindValue(1, $id);
   }
 }
